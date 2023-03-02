d6921a88ca79fd03e93ab0a14fd67e3a71c6c356
Austin <66796130+MacYHarley@users.noreply.github.com>
2023-03-02 15:07:45 -0600

    Update app.py
    deleted the url things

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session, abort
 
+
+
 app = Flask(__name__)
 
 ##Make sure to make a url function...
 url="test.test"
 
-@app.route('/')
+@app.route('/')##, method=["GET","POST"]
 def googleMeet():
-    return render_template('RobolizConsole.html', power = "10%", distance = "10M",error= ["Testing","testing","Test"] )
-    
+    return render_template('RobolizConsole.html', power = "10%", distance = "10m",error= ["Testing","testing","Test"] )
+
+
+
