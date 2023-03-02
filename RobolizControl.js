8285016c17b731207d345033715e828bf57f85ca
Austin <66796130+MacYHarley@users.noreply.github.com>
2023-03-02 16:39:04 -0600

    Update RobolizControl.js
    Fixed a bug

--- RobolizControl.js
+++ RobolizControl.js
@@ -2,7 +2,7 @@
 var Emergency = false;
 var CameraNum= 1;
 var RepeatTime = 100;
-var socket = io();
+//var socket = io();
 
 //AJAX STUFF START UP
 var xhttp = new XMLHttpRequest()
@@ -14,26 +14,22 @@ var FIFTEEN = 0;
 // 1 = forward, 2 = backwards, 3 = rotate right, 4 = rotate left, 5 = Camera right, 6 = Camera left, 7 = Camera Change
 const commands = ["NEW"]; // Start all new chains with NEW, and an END..
 const delays = ["N"]; //N means its a command with no delay, and any other number is in .01 seconds... 
-var up = true;
-var counting = 0;
+
 
 
 
 function mainRunning(){
     if(!Emergency){
         //Put in here the other functions that needs to repeatively happen.
-        console.log("Running Code");        
-        setTimeout(mainRunning,RepeatTime);
+        //console.log("Running Code");        
         if(FIFTEEN == 15){
             FIFTEEN = 0;
-            if(!up){
-                UP();
-                up = false;
-            }
             //Serverpush function
             console.log(commands);
+            
         }FIFTEEN++;
-        if(!up){counting++;}
+        
+        setTimeout(mainRunning,RepeatTime);
     }
 }
 mainRunning();
@@ -42,41 +38,33 @@ mainRunning();
 
 //Handleing inputs:
 function Keys(IN){
-    if(up){
-        if(IN == "W"){
-            commands[commands.length()] = 1;
-        }else if(IN == "A"){
-            commands[commands.length()] = 3;
-        }else if(IN == "S"){
-            commands[commands.length()] = 2;
-        }else if(IN == "D"){
-            commands[commands.length()] = 4;
-        }else if(IN == "J"){
-            commands[commands.length()] = 5;
-        }else if(IN == "K"){
-            commands[commands.length()] = 6;
-        }else if(IN == "P"){
-            commands[commands.length()] = 7;
-            delays[delays.length()] = 'N';
-            up = true;
-            counting = 0;
-            countNOW = false;
-        }
-    }else{
-        //Send up to flask thing saying stop it you dummy... only one key press at a time
+    if (IN == "W") {
+        commands[commands.length] = 1;
+        console.log('1');
+    } else if (IN == "A") {
+        commands[commands.length] = 3;
+        console.log('1');
+    } else if (IN == "S") {
+        commands[commands.length] = 2;
+        console.log('1');
+    } else if (IN == "D") {
+        commands[commands.length] = 4;
+        console.log('1');
+    } else if (IN == "J") {
+        commands[commands.length] = 5;
+        console.log('1');
+    } else if (IN == "K") {
+        commands[commands.length] = 6;
+        console.log('1');
+    } else if (IN == "P") {
+        commands[commands.length] = 7;
+        console.log('7');
+    } else {
+        console.log("UH HO");
     }
-    
+   
 }
 //these start the counting of how long a key is pressed... NOT SPACING OUT WITH LIKE THE 25 Seconds thing...
-function DOWN(){
-    up = false;
-}
-function UP(){
-    up=true;
-    delays[delays.length] = counting;
-    counting = 0;
-    
-}
 
 
 
@@ -129,7 +117,6 @@ function ETJ(err){
     if(!Emergency){Emergency = true;}
     $('#Emergency').text(err);
 }
-
 
 
 
