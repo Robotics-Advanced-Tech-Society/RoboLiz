0d0d6d441130fa0a16f5e002f1983b81b5405e8f
Austin <66796130+MacYHarley@users.noreply.github.com>
2023-03-06 03:38:38 -0600

    Update app.py
    Removing incorrect potion

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session, abort
 
-
-
 app = Flask(__name__)
-
-##Make sure to make a url function...
-url="test.test"
 
 @app.route('/')##, method=["GET","POST"]
 def googleMeet():
     return render_template('RobolizConsole.html', power = "LOADING!", distance = "LOADING!",error= ["Starting Robot","Powering Up","Starting Video link","Robot is booting", "Enjoy your day!"] )
-
 
 
